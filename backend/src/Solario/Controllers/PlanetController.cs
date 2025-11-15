44fdd5c82c967f73da7f884621010e4e99b2e9b4
julnac <julia.naczke007@gmail.com>
2025-11-15 15:42:37 +0100

    setup backend

---
+++ backend/src/Solario/Controllers/PlanetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Solario.Services;
+
+namespace Solario.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlanetController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetWeather()
+    {
+        var service = new PlanetService();
+        var planets = await service.GetPlanets();
+        return Ok(planets);
+    }
+}
